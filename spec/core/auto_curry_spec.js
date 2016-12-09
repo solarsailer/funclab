28ad9bce1724ee8a201a14a5f95dfb013f2db114
Matthieu Oger <moger@pixelnest.io>
2016-12-09 17:18:50 +0100

    Add #autoCurry

---
+++ spec/core/auto_curry_spec.js
@@ -0,0 +1,16 @@
+const autoCurry = require('../../lib/auto_curry')
+
+describe('#autoCurry', () => {
+  var add = autoCurry(function (a, b) { return a + b })
+
+  it('should return the result when used with all parameters', () => {
+    expect(add(2, 2)).toBe(4)
+  })
+
+  it('should return a function if the parameters are not completely provided', () => {
+    expect(typeof add()).toBe('function')
+    expect(typeof add(2)).toBe('function')
+    expect(typeof add(2, 2)).not.toBe('function')
+    expect(typeof add(2, 2, 2)).not.toBe('function')
+  })
+})
